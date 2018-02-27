3704285173a31af80bcdbd3d989750a84db0eaaf
Jean-Nicolas Piketty <jnpiketty@BDLCM.local>
2018-02-27 12:14:00 +0100

    login jwt passport

--- server/app.js
+++ server/app.js
@@ -7,6 +7,12 @@ var bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/Rempart-Vie-DB");
 
+const passport = require("passport");
+const User = require("./models/user");
+require("dotenv").config();
+const config = require("./config");
+const { Strategy, ExtractJwt } = require("passport-jwt");
+
 var app = express();
 
 // view engine setup
@@ -20,6 +26,34 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use(passport.initialize());
+// Create the strategy for JWT
+const strategy = new Strategy(
+  {
+    // this is a config we pass to the strategy
+    // it needs to secret to decrypt the payload of the
+    // token.
+    secretOrKey: config.jwtSecret,
+    // This options tells the strategy to extract the token
+    // from the header of the request
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken()
+  },
+  (payload, done) => {
+    // payload is the object we encrypted at the route /api/token
+    // We get the user id, make sure the user exist by looking it up
+    User.findById(payload.id).then(user => {
+      if (user) {
+        // make the user accessible in req.user
+        done(null, user);
+      } else {
+        done(new Error("User not found"));
+      }
+    });
+  }
+);
+// tell pasport to use it
+passport.use(strategy);
 
 app.use("/", require("./routes/index"));
 app.use("/api", require("./routes/auth"));
